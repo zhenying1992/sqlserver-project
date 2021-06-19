06dc6bad589b299333cf16f5133ba3fa696aa3f1
hou <hou@email.com>
2021-06-19 17:35:27 +0800

    fix

--- front/src/components/home.vue
+++ front/src/components/home.vue
@@ -11,31 +11,38 @@
         <el-menu
           :default-active="activeIndex"
           class="el-menu-vertical-demo"
-          style="height: 600px"
+          style="min-height: 600px"
           mode="vertical"
           @select="handleSelect"
           background-color="#545c64"
           text-color="#fff"
           active-text-color="#ffd04b">
-          <el-menu-item index="1">数据大盘</el-menu-item>
+          <el-menu-item index="1">
+            <i class="el-icon-menu"></i>
+            <span slot="title">数据大盘</span>
+          </el-menu-item>
           <el-submenu index="2">
-            <template slot="title">任务配置</template>
-            <el-menu-item index="2-1">&nbsp定时任务</el-menu-item>
-            <el-menu-item index="2-2">&nbsp人工设置</el-menu-item>
-            <!--        <el-menu-item index="2-3">选项3</el-menu-item>-->
-            <!--        <el-submenu index="2-4">-->
-            <!--          <template slot="title">选项4</template>-->
-            <!--          <el-menu-item index="2-4-1">选项1</el-menu-item>-->
-            <!--          <el-menu-item index="2-4-2">选项2</el-menu-item>-->
-            <!--          <el-menu-item index="2-4-3">选项3</el-menu-item>-->
-            <!--        </el-submenu>-->
+            <template slot="title">
+              <i class="el-icon-monitor"></i>
+              <span slot="title">任务配置</span>
+            </template>
+            <el-menu-item-group>
+              <el-menu-item index="2-1">&nbsp定时任务</el-menu-item>
+              <el-menu-item index="2-2">&nbsp人工设置</el-menu-item>
+            </el-menu-item-group>
           </el-submenu>
-          <el-menu-item index="3">通用工具</el-menu-item>
-          <el-menu-item index="4">日志中心</el-menu-item>
+          <el-menu-item index="3">
+            <i class="el-icon-setting"></i>
+            <span slot="title">通用工具</span>
+          </el-menu-item>
+          <el-menu-item index="4">
+            <i class="el-icon-document"></i>
+            <span slot="title">日志中心</span>
+          </el-menu-item>
         </el-menu>
 
       </el-col>
-      <el-col :span="18">
+      <el-col :span="18" style="height: 600px">
         <div>
           <router-view></router-view>
         </div>
