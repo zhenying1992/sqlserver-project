f698ef63565532e5bfd0f6adc68f6ea410b2a06c
hou <hou@email.com>
2021-12-14 22:40:17 +0800

    fix

--- backend/finance/views/system.py
+++ backend/finance/views/system.py
@@ -1,13 +1,14 @@
 from django.http import JsonResponse
 from finance.views.base import login_require
 from finance.utils import get_disk
+import json
 
 @login_require
 def cpuView(request):
     return JsonResponse({'data': TASK_LIST})
 
 def diskView(request):
-    return JsonResponse({'data': get_disk(), 'status': "success"})
+    return JsonResponse({'data': json.dumps(get_disk()), 'status': "success"})
 
 @login_require
 def memoryView(request):
